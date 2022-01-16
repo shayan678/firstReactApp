17cd1aaadc5df4fc0ba795d02bee22a2fa8092e9
shayan678 <kshayan1234554321@gmail.com>
2022-01-16 20:37:12 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+function App() {
+  return (
+    <div className="container">
+      <button className="fire" >WATER</button>
+      <button className="fire" style={{marginTop: "20px"}} >no no</button>
+
+
+
+    </div>
+  );
+}
+
+export default App;
